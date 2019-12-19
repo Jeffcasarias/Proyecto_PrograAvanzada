b0a56c6f486e17dadbacb9f01aac9677957e2127
usuario <usuario@JEFFERSON_LAP>
2019-12-18 22:33:10 -0600

    Login

---
+++ WebApi_Proyecto_Progra/BLL/Login/cls_Login_BLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL.BD;
+using BLL.BD;
+using DAL.Mant;
+
+namespace BLL.Login
+{
+    public class cls_Login_BLL
+    {
+        cls_BD_BLL Obj_DB_BLL = new cls_BD_BLL();
+        cls_BD_DAL Obj_DB_DAL = new cls_BD_DAL();
+
+        public string ValidarUsuario(ref string User, ref string Password)
+        {
+            Obj_DB_BLL.CrearParametros(ref Obj_DB_DAL);
+            Obj_DB_DAL.DT_Parametros.Rows.Add("@ID_ADMIN", SqlDbType.VarChar, User.ToString().Trim());
+            Obj_DB_DAL.DT_Parametros.Rows.Add("@CONTRASENA", SqlDbType.VarChar, Password.ToString().Trim());
+            Obj_DB_DAL.sTableName = "Validar_Usuario";
+            Obj_DB_DAL.sSP_Name = "dbo.SP_ValidarUsuario";
+
+            Obj_DB_BLL.Execute_DataAdapter(ref Obj_DB_DAL);
+
+            if (Obj_DB_DAL.Obj_DSet.Tables[0].Rows.Count > 0)
+            {
+                return "Login exitoso";
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ WebApi_Proyecto_Progra/WebApi_Proyecto_Progra/Controllers/Login/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL.Login;
+
+namespace WebApi_Proyecto_Progra.Controllers.Login
+{
+    public class LoginController : ApiController
+    {
+        cls_Login_BLL Obj_BLL = new cls_Login_BLL();
+        string Msj = string.Empty;
+
+        // GET: api/Login
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Login/5
+        public string Get(string id)
+        {
+            return "Value";
+        }
+
+        // POST: api/Login
+        public string Post(string IdUser, string Contrasena)
+        {
+            Msj = Obj_BLL.ValidarUsuario(ref IdUser, ref Contrasena);
+
+            if (Msj == null)
+            {
+                return null;
+            }
+            else
+            {
+                return Msj;
+            }
+        }
+
+        // PUT: api/Login/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Login/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
